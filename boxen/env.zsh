64f4ff90217c7cd92d07e1bb9feb49b7a89e2735
Gabe Evans <gabe@ga.be>
2013-11-09 03:14:51 -0800

    Skip boxen in tmux sessions and remove path changes (handled by boxen)

--- boxen/env.zsh
+++ boxen/env.zsh
@@ -1,1 +1,3 @@
-[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
+if [[ -z $TMUX ]]; then
+  [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
+fi
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
-export PATH="./bin:$ZSH/bin:$PATH"
-
+# export PATH="./bin:$ZSH/bin:$PATH"
 # export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
